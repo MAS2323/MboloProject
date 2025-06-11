c3c6261c979f62bc9b474cc71df753dd8402d804
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-06-11 13:26:06 +0800

    mejoras

--- src/components/stores/styles/AppCenterStyle.jsx
+++ src/components/stores/styles/AppCenterStyle.jsx
@@ -6,6 +6,7 @@ const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
     backgroundColor: '#f5f7fa',
+    marginTop: 20, // Adjust for status bar height
   },
   header: {
     flexDirection: 'row',
